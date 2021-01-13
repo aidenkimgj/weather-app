6f1c2d0db98fcc9fa38610e329dc9fbf967ce3b5
Aiden Kim <aidengookjinkim@gmail.com>
2021-01-12 17:13:27 -0700

    changed function type App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
-export default class extends React.Component {
-  state = {
-    isLoading: true,
-  };
+const API_KEY = "b7fd9eb99a43d2167bf33830f871d427";
+
+const App = () => {
+  const [isLoading, setIsLoding] = useState(true);
 
   getLocation = async () => {
     try {
@@ -17,15 +17,15 @@ export default class extends React.Component {
       console.log("latitude:", latitude, "longitude", longitude);
     } catch (error) {
       Alert.alert("Can't find you.");
-      this.setState({ isLoading: false });
+      setIsLoding(false);
     }
   };
 
-  componentDidMount() {
-    this.getLocation();
-  }
-  render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
-  }
-}
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return { isLoading } ? <Loading /> : null;
+};
+
+export default App;
